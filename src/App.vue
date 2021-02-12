d6dcf296b1d25055197739a2489899b688a89219
tanangular <tanangular@gmail.com>
2021-02-12 13:29:38 +0700

    ปรับ index.html title to "Vue 3 + TypeScript + Vite: Template"

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+	<div class="container mx-auto">
+		<img alt="Vue logo" src="./assets/logo.png" class="mx-auto" />
+		<HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+	</div>
 </template>
 
 <script lang="ts">
@@ -16,12 +18,12 @@ export default defineComponent({
 </script>
 
 <style>
-/* #app {
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-} */
+}
 </style>
